ae3bbae0cf2b87c92a7731cbf437914ccc1a0502
Juan Manuel Fresia <jfresia@medallia.com>
2020-03-07 19:38:06 -0800

    Fix wget
    Signed-off-by: Juan Manuel Fresia <jfresia@medallia.com>

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ BASH_COMPLETION_DIR=~/.bash_completion.d
 mkdir -p ${BASH_COMPLETION_DIR}
 
 # Get git-prompt
-wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ${BASH_COMPLETION_DIR}/git-prompt.sh
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -O "${BASH_COMPLETION_DIR}/git-prompt.sh"
 
 cp ./.dircolors ~/.dircolors
 cp ./.bashrc ~/.bashrc
