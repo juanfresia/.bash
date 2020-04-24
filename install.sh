eeaf157543346d5f09a6669f9b4412e1d853ceb4
Juan Manuel Fresia <juanmanuelfresia@gmail.com>
2020-04-24 20:01:35 -0300

    Update for macos
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>

--- install.sh
+++ install.sh
@@ -6,6 +6,5 @@ mkdir -p ${BASH_COMPLETION_DIR}
 # Get git-prompt
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -O "${BASH_COMPLETION_DIR}/git-prompt.sh"
 
-ln -s ~/.bash/.dircolors ~/.dircolors
 ln -s ~/.bash/.bashrc ~/.bashrc
 ln -s ~/.bash/.bash_profile ~/.bash_profile
